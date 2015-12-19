3f579bdf63688366d34f1882875eaf98a9ef98c3
Ziver Koc <ziver@koc.se>
2015-12-19 13:59:45 +0100

    added run script
    Former-commit-id: be675da729024872eef85cd98fbad22996fc4c65

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#screen -S hal -L -d -m \
+#	 java -cp sqlite-jdbc-3.7.2.jar:jSerialComm-1.3.4.jar:hal.jar:. se.koc.hal.plugin.tellstick.TelstickSerialCommTest
+
+#ant clean
+ant build
+screen -S hal -L -d -m ant run-remote-sensor
+
+echo "-----------------"
+screen -list

946511aaab23355317bc479d9dd74540b9a5da03
Ziver Koc <ziver@koc.se>
2016-02-03 17:10:20 +0100

    updated logging
    Former-commit-id: b1bec201615fac36b7aa553fe806bc68535ac0ed

--- src/se/hal/HalServer.java
+++ src/se/hal/HalServer.java
@@ -34,13 +34,15 @@ public class HalServer {
 
     public static void main(String[] args) throws Exception {
         // init logging
-        CompactLogFormatter formatter = new CompactLogFormatter();
+        LogUtil.readConfiguration("logging.properties");
+        /*CompactLogFormatter formatter = new CompactLogFormatter();
         LogUtil.setLevel("se.hal", Level.FINEST);
         LogUtil.setFormatter("se.hal", formatter);
-        LogUtil.setLevel("zutil", Level.FINEST);
         LogUtil.setLevel("zutil.db.bean", Level.INFO);
+        LogUtil.setLevel("zutil.net.http.pages", Level.INFO);
+        LogUtil.setLevel("zutil", Level.FINEST);
         LogUtil.setFormatter("zutil", formatter);
-        LogUtil.setGlobalFormatter(formatter);
+        LogUtil.setGlobalFormatter(formatter);*/
 
         // init Managers
         HalContext.initialize();

606e767f34b7f430a42f758db497e692f8fa9d77
Ziver Koc <ziver@koc.se>
2016-03-14 22:45:44 +0100

    ANT bugfix

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 #	 java -cp sqlite-jdbc-3.7.2.jar:jSerialComm-1.3.4.jar:hal.jar:. se.hal.plugin.tellstick.TelstickSerialCommTest
 
 #ant clean
+ant release
 screen -S hal -L -d -m ant run
 
 echo "-----------------"

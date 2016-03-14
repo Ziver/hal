554dd09e6a6fdc84387749a319a382d3687eb55c
Ziver Koc <ziver@koc.se>
2016-03-14 22:41:43 +0100

    added build dep. to run for ANT

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 #	 java -cp sqlite-jdbc-3.7.2.jar:jSerialComm-1.3.4.jar:hal.jar:. se.hal.plugin.tellstick.TelstickSerialCommTest
 
 #ant clean
-ant build
 screen -S hal -L -d -m ant run
 
 echo "-----------------"

866e776b192d95b2f2fa584f237a03c1d0145eb8
Ziver Koc <ziver@koc.se>
2016-06-29 17:14:05 +0200

    Map view uses sensor and event objects

--- src/se/hal/page/MapHttpPage.java
+++ src/se/hal/page/MapHttpPage.java
@@ -1,6 +1,10 @@
 package se.hal.page;
 
+import se.hal.HalContext;
 import se.hal.intf.HalHttpPage;
+import se.hal.struct.Event;
+import se.hal.struct.Sensor;
+import zutil.db.DBConnection;
 import zutil.io.file.FileUtil;
 import zutil.parser.Templator;
 
@@ -21,6 +25,11 @@ public class MapHttpPage extends HalHttpPage {
 
     @Override
     public Templator httpRespond(Map<String, Object> session, Map<String, String> cookie, Map<String, String> request) throws Exception {
-        return new Templator(FileUtil.find(TEMPLATE));
+        DBConnection db = HalContext.getDB();
+        Templator tmpl = new Templator(FileUtil.find(TEMPLATE));
+        tmpl.set("sensors", Sensor.getLocalSensors(db));
+        tmpl.set("events", Event.getLocalEvents(db));
+
+        return tmpl;
     }
 }

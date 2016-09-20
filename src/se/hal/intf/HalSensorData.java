d099a76ca52860dadf5af210eba2cffe510533a3
Ziver Koc <ziver@koc.se>
2016-09-20 19:05:06 +0200

    Revert: remove HalSensorData

---
+++ src/se/hal/intf/HalSensorData.java
@@ -0,0 +1,10 @@
+package se.hal.intf;
+
+/**
+ * Interface representing one data report from a sensor.
+ *
+ * Created by Ziver on 2016-08-17.
+ */
+public abstract class HalSensorData extends HalDeviceData{
+
+}

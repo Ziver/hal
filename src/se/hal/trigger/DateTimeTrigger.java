df9b41a8431e5264c3b353969bb9af06357ebaf1
Ziver Koc <ziver@koc.se>
2017-01-30 17:17:47 +0100

    Added som initial triggers. issue 43, issue 44

---
+++ src/se/hal/trigger/DateTimeTrigger.java
@@ -0,0 +1,19 @@
+package se.hal.trigger;
+
+import se.hal.intf.HalTrigger;
+
+/**
+ *
+ */
+public class DateTimeTrigger implements HalTrigger {
+
+    @Override
+    public boolean evaluate() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
---
+++ src/se/hal/trigger/TimerTrigger.java
@@ -0,0 +1,26 @@
+package se.hal.trigger;
+
+import se.hal.intf.HalTrigger;
+import zutil.Timer;
+import zutil.ui.Configurator;
+
+/**
+ *
+ */
+public class TimerTrigger implements HalTrigger {
+
+    @Configurator.Configurable("Countdown time (in seconds)")
+    private int timerTime = 10; // default 10s
+    private Timer timer;
+
+
+    @Override
+    public boolean evaluate() {
+        return timer == null || timer.hasTimedOut();
+    }
+
+    @Override
+    public void reset() {
+        timer = new Timer(timerTime * 1000);
+    }
+}

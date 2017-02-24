655de59490e2424a8dd994d4e083c47088b106ab
Ziver Koc <ziver@koc.se>
2017-02-24 18:44:49 +0100

    Added fields to DateTimeTrigger

--- src/se/hal/trigger/DateTimeTrigger.java
+++ src/se/hal/trigger/DateTimeTrigger.java
@@ -1,11 +1,26 @@
 package se.hal.trigger;
 
 import se.hal.intf.HalTrigger;
+import zutil.ui.Configurator;
 
 /**
  *
  */
 public class DateTimeTrigger implements HalTrigger {
+
+    @Configurator.Configurable("Minute (Cron format)")
+    private String minute = "";
+    @Configurator.Configurable("Hour (Cron format)")
+    private String hour = "";
+    @Configurator.Configurable("Day of the Month (Cron format)")
+    private String dayOfMonth = "";
+    @Configurator.Configurable("Month of the Year (Cron format)")
+    private String monthOfYear = "";
+    @Configurator.Configurable("Day of the Week (Cron format)")
+    private String dayOfWeek = "";
+    @Configurator.Configurable("Year (Cron format)")
+    private String year = "";
+
 
     @Override
     public boolean evaluate() {

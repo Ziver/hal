1338f206242872029352b73a0a498c0b73a9451d
Ziver Koc <ziver@koc.se>
2017-03-01 17:53:09 +0100

    Impl date time trigger and updated Zutil

--- src/se/hal/trigger/DateTimeTrigger.java
+++ src/se/hal/trigger/DateTimeTrigger.java
@@ -1,34 +1,48 @@
 package se.hal.trigger;
 
 import se.hal.intf.HalTrigger;
+import zutil.CronTimer;
 import zutil.ui.Configurator;
+import zutil.ui.Configurator.PreConfigurationActionListener;
 
 /**
  *
  */
-public class DateTimeTrigger implements HalTrigger {
+public class DateTimeTrigger implements HalTrigger,PreConfigurationActionListener {
 
     @Configurator.Configurable("Minute (Cron format)")
-    private String minute = "";
+    private String minute = "00";
     @Configurator.Configurable("Hour (Cron format)")
-    private String hour = "";
+    private String hour = "12";
     @Configurator.Configurable("Day of the Month (Cron format)")
-    private String dayOfMonth = "";
-    @Configurator.Configurable("Month of the Year (Cron format)")
-    private String monthOfYear = "";
-    @Configurator.Configurable("Day of the Week (Cron format)")
-    private String dayOfWeek = "";
+    private String dayOfMonth = "*";
+    @Configurator.Configurable("Month (1-12 or Cron format)")
+    private String month = "*";
+    @Configurator.Configurable("Day of the Week (1-7 or Cron format)")
+    private String dayOfWeek = "*";
     @Configurator.Configurable("Year (Cron format)")
-    private String year = "";
+    private String year = "*";
+
+    private transient CronTimer cronTimer;
+    private transient long timeOut = -1;
 
 
     @Override
+    public void preConfigurationAction(Configurator configurator, Object obj) {
+        cronTimer = new CronTimer(minute, hour, dayOfMonth, month, dayOfWeek, year);
+    }
+
+    @Override
     public boolean evaluate() {
-        return false;
+        if (cronTimer == null)
+            return false;
+        if (timeOut < 0)
+            reset();
+        return timeOut < System.currentTimeMillis();
     }
 
     @Override
     public void reset() {
-
+        timeOut = cronTimer.next();
     }
 }

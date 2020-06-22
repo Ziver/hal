659d5ea0d2fb05706921ceda476023333d8b56b7
Ziver Koc <ziver@koc.se>
2020-06-22 14:13:57 +0200

    Updated comment

--- src/se/hal/intf/HalEventReportListener.java
+++ src/se/hal/intf/HalEventReportListener.java
@@ -1,5 +1,8 @@
 package se.hal.intf;
 
+/**
+ * Listener to be called by the {@link HalEventController} to report that a event has been received.
+ */
 public interface HalEventReportListener {
 
     void reportReceived(HalEventConfig e, HalEventData d);
--- src/se/hal/intf/HalSensorReportListener.java
+++ src/se/hal/intf/HalSensorReportListener.java
@@ -1,5 +1,8 @@
 package se.hal.intf;
 
+/**
+ * Listener to be called by the {@link HalSensorController} to report that sensor data has been received.
+ */
 public interface HalSensorReportListener {
 
     void reportReceived(HalSensorConfig s, HalSensorData d);

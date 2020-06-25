47afc78ee45a433946c4820ac26b14ce59a611c8
Ziver Koc <ziver@koc.se>
2020-06-26 00:56:56 +0200

    Moved PC to a plugin directory

---
+++ plugins/hal-powerchallenge/src/se/hal/plugin/powerchallenge/page/PCHeatMapWebPage.java
@@ -0,0 +1,53 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Ziver Koc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package se.hal.plugin.powerchallenge.page;
+
+import se.hal.intf.HalWebPage;
+import zutil.io.file.FileUtil;
+import zutil.parser.Templator;
+
+import java.util.Map;
+
+public class PCHeatMapWebPage extends HalWebPage {
+    private static final String TEMPLATE = "resource/web/pc_heatmap.tmpl";
+
+
+    public PCHeatMapWebPage() {
+        super("pc_heatmap");
+        super.getRootNav().createSubNav("Sensors").createSubNav(this.getId(), "Heatmap").setWeight(60);
+    }
+
+    @Override
+    public Templator httpRespond(
+            Map<String, Object> session,
+            Map<String, String> cookie,
+            Map<String, String> request)
+            throws Exception{
+
+        Templator tmpl = new Templator(FileUtil.find(TEMPLATE));
+        return tmpl;
+    }
+
+}
---
+++ plugins/hal-powerchallenge/src/se/hal/plugin/powerchallenge/page/PCOverviewWebPage.java
@@ -0,0 +1,64 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Ziver Koc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package se.hal.plugin.powerchallenge.page;
+
+import se.hal.HalContext;
+import se.hal.intf.HalWebPage;
+import se.hal.struct.Sensor;
+import se.hal.struct.User;
+import zutil.db.DBConnection;
+import zutil.io.file.FileUtil;
+import zutil.parser.Templator;
+
+import java.util.List;
+import java.util.Map;
+
+public class PCOverviewWebPage extends HalWebPage {
+    private static final String TEMPLATE = "resource/web/pc_overview.tmpl";
+
+    public PCOverviewWebPage() {
+        super("pc_overview");
+        super.getRootNav().createSubNav("Sensors").createSubNav(this.getId(), "Power;Challenge").setWeight(50);
+    }
+
+    @Override
+    public Templator httpRespond (
+            Map<String, Object> session,
+            Map<String, String> cookie,
+            Map<String, String> request)
+            throws Exception {
+
+        DBConnection db = HalContext.getDB();
+        List<Sensor> sensors = Sensor.getSensors(db);
+
+        Templator tmpl = new Templator(FileUtil.find(TEMPLATE));
+        tmpl.set("users", User.getUsers(db));
+        tmpl.set("sensors", sensors);
+
+        return tmpl;
+    }
+
+
+}

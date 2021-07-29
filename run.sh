e57d62471bf5f683027b9c59f0a4e3c2b0ffeb74
Ziver Koc <ziver@koc.se>
2021-07-29 10:30:55 +0100

    Added hal.service file for systemd and updated the run script to support forground execution

--- run.sh
+++ run.sh
@@ -1,12 +1,58 @@
 #!/bin/bash
 
-#ant clean
+# -----------------------------------------------
+# User input
+# -----------------------------------------------
+
+function printHelp() {
+        echo "Run code analysis through SonarQube.
+Usage:
+
+    $(basename $0)
+        [-f|--foreground]
+	[-h|--help]
+
+Where:
+    --foreground        Run Hal Server in the foreground instead of detatching a new screen.
+    --help              Print this help message.
+    "
+}
+
+MODE="SCREEN"
+
+until [[ $# -eq 0 ]]; do
+    case "$1" in
+        -f|--forground)
+            MODE="FORGROUND"
+            shift
+        ;;
+
+        *)
+            echo "ERROR: Unknown input parameter: $1=$2"
+            exit 1
+        ;;
+    esac
+
+    shift
+done
+
+# -----------------------------------------------
+# Execute
+# -----------------------------------------------
+
+# Build Hal source
 ./gradlew build
 
-# Kill current session
-screen -S hal -X kill
-# Start new session
-screen -S hal -L -d -m ./gradlew run
+if [[ ${MODE} == "FORGROUND" ]]; then
+    ./gradlew run
+else 
+    # Kill current session
+    screen -S hal -X kill
+    # Start new session
+    screen -S hal -L -d -m ./gradlew run
 
-echo "-----------------"
-screen -list
+    echo "-------------------------"
+    screen -list
+fi
+
+

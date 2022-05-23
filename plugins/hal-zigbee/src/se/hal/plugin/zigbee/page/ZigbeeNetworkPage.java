95ff5b81c07bd3eb4ed7a4a1948f43d9daf2f02c
Ziver Koc <ziver@koc.se>
2022-05-23 23:19:21 +0200

    Added possibility to refresh individual Zigbee attributes.

--- plugins/hal-zigbee/src/se/hal/plugin/zigbee/page/ZigbeeNetworkPage.java
+++ plugins/hal-zigbee/src/se/hal/plugin/zigbee/page/ZigbeeNetworkPage.java
@@ -1,11 +1,17 @@
 package se.hal.plugin.zigbee.page;
 
+import com.zsmartsystems.zigbee.IeeeAddress;
+import com.zsmartsystems.zigbee.ZigBeeEndpoint;
 import com.zsmartsystems.zigbee.ZigBeeNode;
+import com.zsmartsystems.zigbee.zcl.ZclAttribute;
+import com.zsmartsystems.zigbee.zcl.ZclCluster;
 import se.hal.HalContext;
 import se.hal.intf.HalAbstractController;
 import se.hal.intf.HalAbstractControllerManager;
 import se.hal.intf.HalWebPage;
 import se.hal.plugin.zigbee.ZigbeeController;
+import se.hal.struct.Event;
+import zutil.ObjectUtil;
 import zutil.io.file.FileUtil;
 import zutil.parser.Templator;
 
@@ -28,6 +34,40 @@ public class ZigbeeNetworkPage extends HalWebPage {
             throws Exception {
 
         ZigbeeController controller = HalAbstractControllerManager.getController(ZigbeeController.class);
+
+        if (request.containsKey("action")) {
+            ZigBeeNode node = null;
+            ZigBeeEndpoint endpoint = null;
+            ZclCluster cluster = null;
+            ZclAttribute attribute = null;
+
+            if (!ObjectUtil.isEmpty(request.get("nodeAddress"))) {
+                String nodeAddress = request.get("nodeAddress");
+                node = controller.getNode(new IeeeAddress(nodeAddress));
+
+                if (!ObjectUtil.isEmpty(request.get("endpointId"))) {
+                    int endpointId = Integer.parseInt(request.get("endpointId"));
+                    endpoint = node.getEndpoint(endpointId);
+
+                    if (!ObjectUtil.isEmpty(request.get("clusterId"))) {
+                        int clusterId = Integer.parseInt(request.get("clusterId"));
+                        cluster = endpoint.getInputCluster(clusterId);
+
+                        if (!ObjectUtil.isEmpty(request.get("attributeId"))) {
+                            int attributeId = Integer.parseInt(request.get("attributeId"));
+                            attribute = cluster.getAttribute(attributeId);
+                        }
+                    }
+                }
+            }
+
+            switch (request.get("action")) {
+                case "refresh":
+                    if (attribute != null)
+                        attribute.readValue(0);
+                    break;
+            }
+        }
 
         Templator tmpl = new Templator(FileUtil.find(TEMPLATE));
         tmpl.set("controller", controller);
